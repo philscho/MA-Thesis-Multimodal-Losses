ba7472cdf9bbff3036575428c6fc0718c5fa779a
philscho <phil_scholl@web.de>
2024-09-02 14:20:34 +0200

    Put LightningModule in own file model_module.py Refactor data_module.py for callback (zero shot, linear probe) datasets

--- data_module.py
+++ data_module.py
@@ -19,12 +19,12 @@ class MyDataModule(L.LightningDataModule):
 
     def setup(self, stage=None):
         if stage == "fit":
-            self.train_datasets = self._get_train_datasets()
-            self.val_datasets = self._get_val_datasets()
+            self.train_dataset = self._setup_train_dataset()
+            self.val_dataset = self._setup_val_dataset()
         else:
             raise NotImplementedError(f"Stage {stage} not implemented")
 
-    def _get_train_datasets(self):
+    def _setup_train_dataset(self):
         train_datasets = []
         if "coco" in self.config.dataset.train:
             coco = CocoDataset(
@@ -57,34 +57,57 @@ class MyDataModule(L.LightningDataModule):
 
         return train_all
 
-    def _get_val_datasets(self):
-        val_datasets = {}
+    def _setup_val_dataset(self):
         if "coco_val" in self.config.dataset.val:
-            val_datasets["coco_val"] = CocoDataset(
+            return CocoDataset(
                 self.config.dataset.coco.split_val,
                 os.path.join(self.config.dataset.coco.data_dir, "val2014"),
                 processor=None,
             )
+        else:
+            raise NotImplementedError(f"Val dataset {self.config.dataset.val} not implemented")
+
+    def _setup_callback_datasets(self):
+        datasets = {}
         if "cifar10_val" in self.config.dataset.val:
-            val_datasets["cifar10_val"] = Cifar10Dataset(
+            datasets["cifar10_val"] = Cifar10Dataset(
                 processor=self.processor,
                 **self.config.dataset.cifar10,
             )
         if "caltech101_val" in self.config.dataset.val:
-            val_datasets["caltech101_val"] = Caltech101Dataset(
+            datasets["caltech101_val"] = Caltech101Dataset(
                 processor=self.processor,
                 **self.config.dataset.caltech101,
             )
-        return val_datasets
+        return datasets
 
     def train_dataloader(self):
-        return DataLoader(self.train_datasets, **self.config.dataloader.train, collate_fn=self._collate_fn)
+        return DataLoader(self.train_dataset, **self.config.dataloader.train, collate_fn=self._collate_fn)
 
     def val_dataloader(self):
-        return {
-            key: DataLoader(dataset, **getattr(self.config.dataloader, key), collate_fn=self._collate_fn)
-            for key, dataset in self.val_datasets.items()
-        }
+        return DataLoader(self.val_dataset, **self.config.dataloader.coco_val, collate_fn=self._collate_fn)
+    
+    def callback_dataloader(self):
+        loaders = {}
+        if "cifar10_val" in self.config.dataset.val:
+            loaders["cifar10_val"] = DataLoader(
+                Cifar10Dataset(
+                    processor=self.processor,
+                    **self.config.dataset.cifar10,
+                ),
+                **self.config.dataloader.cifar10_val,
+                collate_fn=self._collate_fn,
+            )
+        if "caltech101_val" in self.config.dataset.val:
+            loaders["caltech101_val"] = DataLoader(
+                Caltech101Dataset(
+                    processor=self.processor,
+                    **self.config.dataset.caltech101,
+                ),
+                **self.config.dataloader.caltech101_val,
+                collate_fn=self._collate_fn,
+            )
+        return loaders
 
     # TODO: not sure if custom collate_fn works with pinned_memory in dataloader.
     # return type of processor is custom dict type, not sure if it works with pinned_memory
